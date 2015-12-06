c1a78fadd4f401dbae61afb733148d81c3637d9d
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-12-06 12:36:10 -0800

    updated Package

--- Package.swift
+++ Package.swift
@@ -4,5 +4,12 @@ let package = Package(
     name: "SwiftBlueZ",
     dependencies: [
         .Package(url: "https://github.com/PureSwift/CBlueZ.git", majorVersion: 1),
+    ],
+    targets: [
+        Target(
+            name: "UnitTests",
+            dependencies: [.Target(name: "SwiftBlueZ")]),
+        Target(
+            name: "SwiftBlueZ")
     ]
 )
