2214b1db2f606d072bf7d15bac8e23c96f8e6cb0
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-01-14 09:44:24 -0500

    added more LinkControl commands

--- Sources/BlueZ/LinkControlCommand.swift
+++ Sources/BlueZ/LinkControlCommand.swift
@@ -13,55 +13,82 @@ public extension Bluetooth {
         public static let opcodeGroupField = OpcodeGroupField.LinkControl
         
         /// Command used to enter Inquiry mode where it discovers other Bluetooth devices.
-        case Inquiry                    = 0x01
+        case Inquiry                    = 0x0001
         
         /// Command to cancel the Inquiry mode in which the Bluetooth device is in.
-        case InquiryCancel              = 0x02
+        case InquiryCancel              = 0x0002
         
         /// Command to set the device to enter Inquiry modes periodically according to the time interval set.
-        case PeriodicInquiry            = 0x03
+        case PeriodicInquiry            = 0x0003
         
         /// Command to exit the periodic Inquiry mode.
-        case ExitPeriodicInquiry        = 0x04
+        case ExitPeriodicInquiry        = 0x0004
         
         /// Command to create an ACL connection to the device specified by the BD_ADDR in the parameters.
-        case CreateConnection           = 0x05
+        case CreateConnection           = 0x0005
         
         /// Command to terminate the existing connection to a device.
-        case Disconnect                 = 0x06
+        case Disconnect                 = 0x0006
         
         /// Create an SCO connection defined by the connection handle parameters.
-        case AddSCOConnection           = 0x07
+        case AddSCOConnection           = 0x0007
         
         /// Cancel Create Connection
-        case CreateConnectionCancel     = 0x08
+        case CreateConnectionCancel     = 0x0008
         
         /// Command to accept a new connection request.
-        case AcceptConnection           = 0x09
+        case AcceptConnection           = 0x0009
         
         /// Command to reject a new connection request.
-        case RejectConnection           = 0x0A
+        case RejectConnection           = 0x000A
         
         /// Reply command to a link key request event sent from controller to the host.
-        case LinkKeyReply               = 0x0B
+        case LinkKeyReply               = 0x000B
         
         /// Reply command to a link key request event from the controller to the host if there is no link key associated with the connection.
-        case LinkKeyNegativeReply       = 0x0C
+        case LinkKeyNegativeReply       = 0x000C
         
         /// Reply command to a PIN code request event sent from a controller to the host.
-        case PinCodeReply               = 0x0D
+        case PinCodeReply               = 0x000D
         
         /// Reply command to a PIN code request event sent from the controller to the host if there is no PIN associated with the connection.
-        case PinCodeNegativeReply       = 0x0E
+        case PinCodeNegativeReply       = 0x000E
         
         /// Command to change the type of packets to be sent for an existing connection.
-        case SetConnectionPacketType    = 0x0F
+        case SetConnectionPacketType    = 0x000F
         
         /// Command to establish authentication between two devices specified by the connection handle.
-        case AuthenticationRequested    = 0x11
+        case AuthenticationRequested    = 0x0011
         
-        /// 
-        case
+        /// Command to enable or disable the link level encryption.
+        case SetConnectionEncryption    = 0x0013
+        
+        /// Command to force the change of a link key to a new one between two connected devices.
+        case ChangeConnectionLinkKey    = 0x0015
+        
+        /// Command to force two devices to use the master's link key temporarily.
+        case MasterLinkKey              = 0x0017
+        
+        /// Command to determine the user friendly name of the connected device.
+        case RemoteNameRequest          = 0x0019
+        
+        /// Cancels the remote name request.
+        case RemoteNameRequestCancel    = 0x001A
+        
+        /// Command to determine the features supported by the connected device.
+        case ReadRemoteFeatures         = 0x001B
+        
+        /// Command to determine the extended features supported by the connected device.
+        case ReadRemoteExtendedFeatures = 0x001C
+        
+        /// Command to determine the version information of the connected device.
+        case ReadRemoteVersion          = 0x001D
+        
+        /// Command to read the clock offset of the remote device.
+        case ReadClockOffset            = 0x001F
+        
+        /// Read LMP Handle
+        case ReadLMPHandle              = 0x0020
     }
 }
 
