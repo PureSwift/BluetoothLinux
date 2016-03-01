0bc39b3109a633c0140e61fc9ff1dd0cbebbaed8
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-03-01 02:20:53 -0500

    added missing ATTOpcode

--- Sources/BlueZ/ATT.swift
+++ Sources/BlueZ/ATT.swift
@@ -43,6 +43,8 @@ public struct ATT {
     
     public typealias Opcode                             = ATTOpcode
     
+    public typealias OpcodeType                         = ATTOpcodeType
+    
     public typealias AttributePermission                = ATTAttributePermission
 }
 
@@ -68,20 +70,111 @@ public enum ATTError: UInt8, ErrorType {
 /// ATT protocol opcodes.
 public enum ATTOpcode: UInt8 {
     
+    // Masks
     public static let CommandMask                   = 0x40
     public static let SignedMask                    = 0x80
     
+    /// Error response
     case ErrorResponse                              = 0x01
+    
+    // Exchange MTU
     case MaximumTransmissionUnitRequest             = 0x02
     case MaximumTransmissionUnitResponse            = 0x03
+    
+    // Find Information
     case FindInformationRequest                     = 0x04
     case FindInformationResponse                    = 0x05
+    
+    // Find By Type Value
     case FindByTypeRequest                          = 0x06
     case FindByTypeResponse                         = 0x07
+    
+    // Read By Type
     case ReadByTypeRequest                          = 0x08
     case ReadByTypeResponse                         = 0x09
     
-    // TODO: All Opcodes
+    // Read
+    case ReadRequest                                = 0x0a
+    case ReadResponse                               = 0x0b
+    
+    // Read Blob
+    case ReadBlobRequest                            = 0x0c
+    case ReadBlobResponse                           = 0x0d
+    
+    // Read Multiple
+    case ReadMultipleRequest                        = 0x0e
+    case ReadMultipleResponse                       = 0x0f
+    
+    // Read By Group Type
+    case ReadByGroupTypeRequest                     = 0x10
+    case ReadByGroupTypeResponse                    = 0x11
+    
+    // Write
+    case WriteRequest                               = 0x12
+    case WriteResponse                              = 0x13
+    case WriteCommand                               = 0x52
+    case SignedWriteCommand                         = 0xD2
+    
+    // Prepare Write
+    case PreparedWriteRequest                       = 0x16
+    case PreparedWriteResponse                      = 0x17
+    
+    // Execute Write
+    case ExecuteWriteRequest                        = 0x18
+    case ExecuteWriteResponse                       = 0x19
+    
+    // Handle Value
+    case HandleValueNotification                    = 0x1B
+    case HandleValueIndication                      = 0x1D
+    case HandleValueConfirmation                    = 0x1E
+    
+    /// Specifies the opcode category.
+    public var type: ATT.OpcodeType {
+        
+        switch self {
+        
+        case .ErrorResponse:                    return .Response
+        case .MaximumTransmissionUnitRequest:   return .Request
+        case .MaximumTransmissionUnitResponse:  return .Response
+        case .FindInformationRequest:           return .Request
+        case .FindInformationResponse:          return .Response
+        case .FindByTypeRequest:                return .Request
+        case .FindByTypeResponse:               return .Response
+        case .ReadByTypeRequest:                return .Request
+        case .ReadByTypeResponse:               return .Response
+        case .ReadRequest:                      return .Request
+        case .ReadResponse:                     return .Response
+        case .ReadBlobRequest:                  return .Request
+        case .ReadBlobResponse:                 return .Response
+        case .ReadMultipleRequest:              return .Request
+        case .ReadMultipleResponse:             return .Response
+        case .ReadByGroupTypeRequest:           return .Request
+        case .ReadByGroupTypeResponse:          return .Response
+        case .WriteRequest:                     return .Request
+        case .WriteResponse:                    return .Response
+        case .WriteCommand:                     return .Command
+        case .SignedWriteCommand:               return .Command
+        case .PreparedWriteRequest:             return .Request
+        case .PreparedWriteResponse:            return .Response
+        case .ExecuteWriteRequest:              return .Request
+        case .ExecuteWriteResponse:             return .Response
+        case .HandleValueNotification:          return .Notification
+        case .HandleValueIndication:            return .Indication
+        case .HandleValueConfirmation:          return .Confirmation
+        
+        }
+    }
+}
+
+/// ATT protocol opcode categories.
+public enum ATTOpcodeType {
+    
+    case Request
+    case Response
+    case Command
+    case Indication
+    case Notification
+    case Confirmation
 }
 
 /// ATT attribute permission bitfield values. Permissions are grouped as
