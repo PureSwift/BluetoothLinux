0f1a1caed56a202600d13545197ce01284d13adf
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-03-01 01:06:58 -0500

    fixed ATT protocol namespacing

--- Sources/BlueZ/ATT.swift
+++ Sources/BlueZ/ATT.swift
@@ -35,70 +35,77 @@ public struct ATT {
         }
     }
     
-    /// ATT protocol security
-    public enum Security: CInt {
-        
-        case Auto       = 0
-        case Low        = 1
-        case Medium     = 2
-        case High       = 3
-    }
+    // Namespace Typealiases
     
-    /// ATT protocol opcodes.
-    public enum Opcode: UInt8 {
-        
-        public static let CommandMask                   = 0x40
-        public static let SignedMask                    = 0x80
-        
-        case ErrorResponse                              = 0x01
-        case MaximumTransmissionUnitRequest             = 0x02
-        case MaximumTransmissionUnitResponse            = 0x03
-        case FindInformationRequest                     = 0x04
-        case FindInformationResponse                    = 0x05
-        case FindByTypeRequest                          = 0x06
-        case FindByTypeResponse                         = 0x07
-        case ReadByTypeRequest                          = 0x08
-        case ReadByTypeResponse                         = 0x09
-        
-        // TODO: All Opcodes
-    }
+    public typealias Security                           = ATTSecurity
     
-    /// Error codes for Error response PDU.
-    public enum Error: UInt8 {
-        
-        case InvalidHandle                              = 0x01
-        case ReadNotPermitted                           = 0x02
-        
-        // TODO: All Error Codes
-        
-    }
+    public typealias Error                              = ATTError
     
-    /// ATT attribute permission bitfield values. Permissions are grouped as
-    /// "Access", "Encryption", "Authentication", and "Authorization". A bitmask of
-    /// permissions is a byte that encodes a combination of these.
-    public enum AttributePermission: UInt8 {
-        
-        // Access
-        case Read                                       = 0x01
-        case Write                                      = 0x02
-        
-        // Encryption
-        case Encrypt                                    = 12 // ReadEncrypt | WriteEncrypt
-        case ReadEncrypt                                = 0x04
-        case WriteEncrypt                               = 0x08
-        
-        // Authentication
-        case Authentication                             = 48 // ReadAuthentication | WriteAuthentication
-        case ReadAuthentication                         = 0x10
-        case WriteAuthentication                        = 0x20
-        
-        // Authorization
-        case Authorized                                 = 0x40
-        case None                                       = 0x80
-    }
+    public typealias Opcode                             = ATTOpcode
+    
+    public typealias AttributePermission                = ATTAttributePermission
+}
+
+/// ATT protocol security
+public enum ATTSecurity: CInt {
+    
+    case Auto       = 0
+    case Low        = 1
+    case Medium     = 2
+    case High       = 3
+}
+
+/// Error codes for Error response PDU.
+public enum ATTError: UInt8, ErrorType {
+    
+    case InvalidHandle                              = 0x01
+    case ReadNotPermitted                           = 0x02
+    
+    // TODO: All Error Codes
     
 }
 
+/// ATT protocol opcodes.
+public enum ATTOpcode: UInt8 {
+    
+    public static let CommandMask                   = 0x40
+    public static let SignedMask                    = 0x80
+    
+    case ErrorResponse                              = 0x01
+    case MaximumTransmissionUnitRequest             = 0x02
+    case MaximumTransmissionUnitResponse            = 0x03
+    case FindInformationRequest                     = 0x04
+    case FindInformationResponse                    = 0x05
+    case FindByTypeRequest                          = 0x06
+    case FindByTypeResponse                         = 0x07
+    case ReadByTypeRequest                          = 0x08
+    case ReadByTypeResponse                         = 0x09
+    
+    // TODO: All Opcodes
+}
 
+/// ATT attribute permission bitfield values. Permissions are grouped as
+/// "Access", "Encryption", "Authentication", and "Authorization". A bitmask of
+/// permissions is a byte that encodes a combination of these.
+public enum ATTAttributePermission: UInt8 {
+    
+    // Access
+    case Read                                       = 0x01
+    case Write                                      = 0x02
+    
+    // Encryption
+    case Encrypt                                    = 12 // ReadEncrypt | WriteEncrypt
+    case ReadEncrypt                                = 0x04
+    case WriteEncrypt                               = 0x08
+    
+    // Authentication
+    case Authentication                             = 48 // ReadAuthentication | WriteAuthentication
+    case ReadAuthentication                         = 0x10
+    case WriteAuthentication                        = 0x20
+    
+    // Authorization
+    case Authorized                                 = 0x40
+    case None                                       = 0x80
+}
 
 
