23db475d3b8bd0a057874918e8335d689bc3c75d
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-03-04 03:39:55 -0500

    working on GATTDatabase

--- Sources/BluetoothLinux/GATTDatabase.swift
+++ Sources/BluetoothLinux/GATTDatabase.swift
@@ -7,7 +7,76 @@
 //
 
 /// GATT Database
-public final class GATTDatabase {
+public struct GATTDatabase {
     
+    // MARK: - Private Properties
     
+    private var services = Deque<Service>()
+    
+    private var notifyList = Deque<Notify>()
+    
+    private var nextHandle: UInt16 = 0x0001
+    
+    // MARK: - Initialization
+    
+    public init() { }
+    
+    // MARK: - Methods
+    
+    public mutating func clear() {
+        
+        self.nextHandle = 0
+        self.services.removeAll()
+    }
+    
+    public mutating func removeService(attribute: Attribute) {
+        
+        
+    }
+    
+    // MARK: - Dynamic Properties
+    
+    public var isEmpty: Bool {
+        
+        return services.isEmpty
+    }
 }
+
+// MARK: - Supporting Types
+
+public extension GATTDatabase {
+    
+    public struct Service {
+        
+        public var attributes = [Attribute]()
+        
+        public var active = false
+        
+        public var claimed = false
+        
+        public init() { }
+    }
+    
+    public struct Attribute {
+        
+        
+    }
+}
+
+// MARK: - Internal Types
+
+internal extension GATTDatabase {
+    
+    struct Notify {
+        
+        let identifier: UInt
+        
+        let serviceAdded: () -> ()
+        
+        let serviceRemoved: () -> ()
+        
+        
+    }
+}
+
+
