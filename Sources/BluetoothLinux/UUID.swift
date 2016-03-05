97a220dc17f147bc146e1a18949cf528c634e3d8
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-03-05 05:59:41 -0500

    added BluetoothUUID.toUUID()
    for conversion to SwiftFoundation.UUID

--- Sources/BluetoothLinux/UUID.swift
+++ Sources/BluetoothLinux/UUID.swift
@@ -11,6 +11,10 @@ import struct SwiftFoundation.UUID
 /// Bluetooth UUID
 public enum BluetoothUUID {
     
+    /// Bluetooth Base UUID
+    public static let BaseUUID = UUID(byteValue: (0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x00,
+        0x80, 0x00, 0x00, 0x80, 0x5F, 0x9B, 0x34, 0xFB))
+    
     case Bit16(UInt16)
     case Bit128(SwiftFoundation.UUID)
     
@@ -29,6 +33,27 @@ public enum BluetoothUUID {
             let bytes = value.byteValue
             
             return [bytes.0, bytes.1, bytes.2, bytes.3, bytes.4, bytes.5, bytes.6, bytes.7, bytes.8, bytes.9, bytes.10, bytes.11, bytes.12, bytes.13, bytes.14, bytes.15]
+        }
+    }
+    
+    /// Converts the Bluetooth UUID to a universal UUID.
+    public func toUUID() -> SwiftFoundation.UUID {
+        
+        switch self {
+            
+        case let .Bit128(value): return value
+            
+        case let .Bit16(value):
+            
+            let bytes = value.littleEndianBytes
+            
+            var byteValue = BluetoothUUID.BaseUUID.byteValue
+            
+            // replace empty bytes with UInt16 bytes
+            byteValue.0 = bytes.0
+            byteValue.1 = bytes.1
+            
+            return SwiftFoundation.UUID(byteValue: byteValue)
         }
     }
 }
