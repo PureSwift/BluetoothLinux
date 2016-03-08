c3aa782f3f8cfe4082a422322b0e0bc80b9492eb
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-03-08 16:49:50 -0500

    #9 Added ATTPrepareWriteResponse

--- Sources/BluetoothLinux/ATTProtocolDataUnit.swift
+++ Sources/BluetoothLinux/ATTProtocolDataUnit.swift
@@ -1574,5 +1574,67 @@ public struct ATTPrepareWriteRequest: ATTProtocolDataUnit {
     }
 }
 
-
+/// Prepare Write Response
+/// The *Prepare Write Response* is sent in response to a received *Prepare Write Request*
+/// and acknowledges that the value has been successfully received and placed in the prepare write queue.
+public struct ATTPrepareWriteResponse: ATTProtocolDataUnit {
+    
+    public static let attributeOpcode = ATT.Opcode.PreparedWriteResponse
+    
+    /// Minimum length
+    public static let length = 1 + 2 + 2 + 0
+    
+    /// The handle of the attribute to be written.
+    public var handle: UInt16
+    
+    /// The offset of the first octet to be written.
+    public var offset: UInt16
+    
+    /// The value of the attribute to be written.
+    public var partValue: [UInt8]
+    
+    public init(handle: UInt16 = 0, offset: UInt16 = 0, partValue: [UInt8] = []) {
+        
+        self.handle = handle
+        self.offset = offset
+        self.partValue = partValue
+    }
+    
+    public init?(byteValue: [UInt8]) {
+            
+        let type = ATTPrepareWriteRequest.self
+            
+        guard byteValue.count >= type.length
+            else { return nil }
+            
+        let attributeOpcodeByte = byteValue[0]
+            
+        guard attributeOpcodeByte == type.attributeOpcode.rawValue
+            else { return nil }
+            
+        self.handle = UInt16(littleEndian: (byteValue[1], byteValue[2]))
+            
+        self.offset = UInt16(littleEndian: (byteValue[3], byteValue[4]))
+            
+        if byteValue.count > type.length {
+                
+            self.partValue = Array(byteValue.suffixFrom(5))
+                
+        } else {
+                
+            self.partValue = []
+        }
+    }
+    
+    public var byteValue: [UInt8] {
+        
+        let type = ATTSignedWriteCommand.self
+                
+        let handleBytes = handle.littleEndianBytes
+                
+        let offsetBytes = offset.littleEndianBytes
+                
+        return [type.attributeOpcode.rawValue, handleBytes.0, handleBytes.1, offsetBytes.0, offsetBytes.1] + partValue
+    }
+}
 
