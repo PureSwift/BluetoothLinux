c6fd742e8bf19060cc143cc6b033db3f20e6d9b3
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-12-01 01:12:14 -0500

    Specified unit test dependencies

--- Package.swift
+++ Package.swift
@@ -24,22 +24,18 @@ let package = Package(
         Target(
             name: "BluetoothLinux",
             dependencies: [
-                .Target(name: "CSwiftBluetoothLinux"),
-                .Target(name: "CSwiftBluetoothLinuxTest")
+                .Target(name: "CSwiftBluetoothLinux")
             ]),
         Target(
             name: "CSwiftBluetoothLinux"),
         Target(
-            name: "CSwiftBluetoothLinuxTest")
-        
-        /*
+            name: "CSwiftBluetoothLinuxTest"),
         Target(
             name: "BluetoothLinuxTests",
             dependencies: [
                 .Target(name: "BluetoothLinux"),
                 .Target(name: "CSwiftBluetoothLinuxTest")
             ])
-        */
     ],
     dependencies: [
         .Package(url: "https://github.com/PureSwift/Bluetooth.git", majorVersion: 1)
