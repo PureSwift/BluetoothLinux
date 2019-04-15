66466d7656350b755bb179bd7b6dbb83d14dcf66
Alsey Coleman Miller <alseycmiller@gmail.com>
2019-04-14 22:44:19 -0500

    Fixed `POSIXError` for Linux
    https://github.com/apple/swift/pull/24028
    
    https://github.com/apple/swift-corelibs-foundation/pull/2113

--- Sources/BluetoothLinux/POSIXError.swift
+++ Sources/BluetoothLinux/POSIXError.swift
@@ -7,15 +7,42 @@
 //
 
 import Foundation
+import Bluetooth
+
+#if canImport(MSVCRT)
+import MSVCRT
+import WinSDK
+#elseif canImport(Darwin)
+import Darwin
+#elseif canImport(Glibc)
+import Glibc
+#endif
 
 internal extension POSIXError {
     
     /// Creates error from C ```errno```.
     static var fromErrno: POSIXError? {
         
-        guard let code = POSIXError.Code(rawValue: POSIXError.Code.RawValue(errno))
+        guard let code = POSIXErrorCode(rawValue: errno)
             else { return nil }
         
         return self.init(code)
+    }
+    
+    /// Initializes `POSIXError` from an error code.
+    init(_ errorCode: POSIXErrorCode) {
+        
+        var userInfo = [String: Any](minimumCapacity: 1)
+        
+        if let description = String(cString: strerror(errorCode.rawValue), encoding: .ascii) {
+            userInfo[NSLocalizedDescriptionKey] = description
+        }
+        
+        let nsError = NSError(
+            domain: NSPOSIXErrorDomain,
+            code: Int(errorCode.rawValue),
+            userInfo: userInfo)
+        
+        self.init(_nsError: nsError)
     }
 }
