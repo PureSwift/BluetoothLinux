6d6369804ead4ba0364e03a5f2e3dff1581fa30a
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-05-08 10:50:45 -0500

    Deprecated Swift 4 support

--- Package.swift
+++ Package.swift
@@ -1,20 +1,37 @@
-// swift-tools-version:4.1
+// swift-tools-version:5.0
 import PackageDescription
 
-_ = Package(name: "BluetoothLinux",
-            products: [
-                .library(
-                    name: "BluetoothLinux",
-                    targets: ["BluetoothLinux"]
-                )
-            ],
+let package = Package(
+    name: "BluetoothLinux",
+    products: [
+        .library(
+            name: "BluetoothLinux",
+            targets: ["BluetoothLinux"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/PureSwift/Bluetooth.git",
+            .branch("master")
+        )
+    ],
+    targets: [
+        .target(
+            name: "BluetoothLinux",
+            dependencies: ["Bluetooth", "CSwiftBluetoothLinux"]
+        ),
+        .target(
+            name: "CSwiftBluetoothLinux"
+        ),
+        .target(
+            name: "CSwiftBluetoothLinuxTest"
+        ),
+        .testTarget(
+            name: "BluetoothLinuxTests",
             dependencies: [
-                .package(url: "https://github.com/PureSwift/Bluetooth.git", .branch("master"))
-            ],
-            targets: [
-                .target(name: "BluetoothLinux", dependencies: ["Bluetooth", "CSwiftBluetoothLinux"]),
-                .target(name: "CSwiftBluetoothLinux"),
-                .target(name: "CSwiftBluetoothLinuxTest"),
-                .testTarget(name: "BluetoothLinuxTests", dependencies: ["BluetoothLinux", "CSwiftBluetoothLinuxTest"])
-            ],
-            swiftLanguageVersions: [4])
+                "BluetoothLinux",
+                "CSwiftBluetoothLinuxTest"
+            ]
+        )
+    ]
+)
