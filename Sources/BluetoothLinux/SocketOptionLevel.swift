948044f3ce4c435f27f85aea2bab3958db876f8a
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-10-16 18:19:13 -0500

    Added `SocketOptionLevel` definitions

---
+++ Sources/BluetoothLinux/SocketOptionLevel.swift
@@ -0,0 +1,27 @@
+//
+//  SocketOptionLevel.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 16/10/21.
+//
+
+import SystemPackage
+
+public extension SocketOptionLevel {
+    
+    /// Bluetooth HCI Socket Option Level
+    @_alwaysEmitIntoClient
+    static var hostControllerInterface: SocketOptionLevel { SocketOptionLevel(rawValue: 0) }
+    
+    /// Bluetooth L2CAP Socket Option Level
+    @_alwaysEmitIntoClient
+    static var l2cap: SocketOptionLevel { SocketOptionLevel(rawValue: 6) }
+    
+    /// Bluetooth SCO Socket Option Level
+    @_alwaysEmitIntoClient
+    static var sco: SocketOptionLevel { SocketOptionLevel(rawValue: 17) }
+    
+    /// Bluetooth RFCOMM Socket Option Level
+    @_alwaysEmitIntoClient
+    static var rfcomm: SocketOptionLevel { SocketOptionLevel(rawValue: 18) }
+}
