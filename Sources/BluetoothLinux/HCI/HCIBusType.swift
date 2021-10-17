7ae756afc25be46b7b243028323bf04dfb432efb
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-10-16 19:08:27 -0500

    Updated `HCIBusType`

--- Sources/BluetoothLinux/HCI/HCIBusType.swift
+++ Sources/BluetoothLinux/HCI/HCIBusType.swift
@@ -8,11 +8,15 @@
 /// HCI bus types
 public enum HCIBusType: CInt {
     
-    case virtual
-    case usb
-    case pcCard
-    case uart
-    case rs232
-    case pci
-    case sdio
+    case virtual    = 0
+    case usb        = 1
+    case pcCard     = 2
+    case uart       = 3
+    case rs232      = 4
+    case pci        = 5
+    case sdio       = 6
+    case spi        = 7
+    case i2c        = 8
+    case smd        = 9
+    case virtio     = 10
 }
