d072014b16e0af8341d6bb1ed88c532dbabae2ad
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-10-16 19:03:11 -0500

    Updated `BluetoothSocketOption`

--- Sources/BluetoothLinux/SocketOption.swift
+++ Sources/BluetoothLinux/SocketOption.swift
@@ -15,11 +15,11 @@ public enum BluetoothSocketOption: CInt, SocketOptionID {
     
     /// Bluetooth Security socket option
     case security       = 4 // BT_SECURITY
-    
-    /// Defer setup boolean option
+
+    /// Bluetooth defer setup socket option
     case deferSetup     = 7 // BT_DEFER_SETUP
     
-    /// Flushable boolean option
+    /// Bluetooth flushable socket option
     case flushable      = 8
     
     /// Bluetooth power socket option
@@ -27,6 +27,24 @@ public enum BluetoothSocketOption: CInt, SocketOptionID {
     
     /// Bluetooth Channel Policy socket option
     case channelPolicy  = 10 // BT_CHANNEL_POLICY
+    
+    /// Bluetooth Voice options
+    case voice          = 11 // BT_VOICE
+    
+    /// Bluetooth Socket Send MTU
+    case sendMTU        = 12 // BT_SNDMTU
+    
+    /// Bluetooth Socket Recieve MTU
+    case recieveMTU     = 13 // BT_RCVMTU
+    
+    /// Bluetooth Phy
+    case phy            = 14 // BT_PHY
+    
+    /// Bluetooth Mode
+    case mode           = 15 // BT_MODE
+    
+    /// Bluetooth Packet Status
+    case packetStatus   = 16 // BT_PKT_STATUS
 }
 
 public extension BluetoothSocketOption {
@@ -66,4 +84,9 @@ public extension BluetoothSocketOption {
             return Self.init(value)
         }
     }
+}
+
+public extension BluetoothSocketOption {
+    
+    
 }
