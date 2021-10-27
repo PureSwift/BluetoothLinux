489a7995233d957b6ab5c6f2f5954f18fa12d44c
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-10-27 10:45:32 -0500

    Added `RFCOMMFlag`

--- Sources/BluetoothLinux/RFCOMM/RFCOMMFlag.swift
+++ Sources/BluetoothLinux/RFCOMM/RFCOMMFlag.swift
@@ -5,4 +5,13 @@
 //  Created by Alsey Coleman Miller on 27/10/21.
 //
 
-import Foundation
+import Bluetooth
+
+/// RFCOMM Flags
+public enum RFCOMMFlag: UInt32, CaseIterable, BitMaskOption {
+    
+    case reuseDLC           = 0x01 // RFCOMM_REUSE_DLC
+    case releaseOnHangup    = 0x02 // RFCOMM_RELEASE_ONHUP
+    case hangupNow          = 0x04 // RFCOMM_HANGUP_NOW
+    case serialAttached     = 0x08 // RFCOMM_TTY_ATTACHED
+}
