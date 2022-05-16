385eb4344ee6284fbe3b9c8630d64ef62962399c
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-05-16 00:23:26 -0700

    Updated `HCIBusType`

--- Sources/BluetoothLinux/HCI/HCIBusType.swift
+++ Sources/BluetoothLinux/HCI/HCIBusType.swift
@@ -6,17 +6,92 @@
 //
 
 /// HCI bus types
-public enum HCIBusType: CInt {
+@frozen
+public struct HCIBusType: RawRepresentable, Equatable, Hashable {
     
-    case virtual    = 0
-    case usb        = 1
-    case pcCard     = 2
-    case uart       = 3
-    case rs232      = 4
-    case pci        = 5
-    case sdio       = 6
-    case spi        = 7
-    case i2c        = 8
-    case smd        = 9
-    case virtio     = 10
+    public let rawValue: CInt
+    
+    public init(rawValue: CInt) {
+        self.rawValue = rawValue
+    }
+    
+    private init(_ raw: CInt) {
+        self.init(rawValue: raw)
+    }
+}
+
+// MARK: - Definitions
+
+public extension HCIBusType {
+    
+    /// Bluetooth Virtual Bus Type
+    static var virtual: HCIBusType       { HCIBusType(0) }
+    
+    /// Bluetooth USB controller type
+    static var usb: HCIBusType           { HCIBusType(1) }
+    
+    /// Bluetooth PCCARD controller type
+    static var pcCard: HCIBusType        { HCIBusType(2) }
+    
+    /// Bluetooth UART controller type
+    static var uart: HCIBusType          { HCIBusType(3) }
+    
+    /// Bluetooth RS232 controller type
+    static var rs232: HCIBusType         { HCIBusType(4) }
+    
+    /// Bluetooth PCI controller type
+    static var pci: HCIBusType           { HCIBusType(5) }
+    
+    /// Bluetooth SDIO controller type
+    static var sdio: HCIBusType          { HCIBusType(6) }
+    
+    /// Bluetooth SPI controller type
+    static var spi: HCIBusType           { HCIBusType(7) }
+    
+    /// Bluetooth I2C controller type
+    static var i2c: HCIBusType           { HCIBusType(8) }
+    
+    /// Bluetooth SMD controller type
+    static var smd: HCIBusType           { HCIBusType(9) }
+    
+    /// Bluetooth VIRTIO controller type
+    static var virtio: HCIBusType        { HCIBusType(10) }
+}
+
+// MARK: - Definitions
+
+extension HCIBusType: CustomStringConvertible, CustomDebugStringConvertible {
+    
+    public var description: String {
+        switch self {
+        case .virtual:
+            return "Virtual"
+        case .usb:
+            return "USB"
+        case .pcCard:
+            return "PCCARD"
+        case .uart:
+            return "UART"
+        case .rs232:
+            return "RS232"
+        case .pci:
+            return "PCI"
+        case .sdio:
+            return "SDIO"
+        case .spi:
+            return "SPI"
+        case .i2c:
+            return "I2C"
+        case .smd:
+            return "SMD"
+        case .virtio:
+            return "VIRTIO"
+        default:
+            return "Unknown \(rawValue)"
+        }
+    }
+    
+    public var debugDescription: String {
+        description
+    }
 }
