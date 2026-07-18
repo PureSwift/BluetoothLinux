da97f8aeffad3a4e7d16d5320a544c4156461c36
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-17 23:33:24 -0400

    Fix RFCOMM device destination address mapping

--- Sources/BluetoothLinux/RFCOMM/RFCOMMDevice.swift
+++ Sources/BluetoothLinux/RFCOMM/RFCOMMDevice.swift
@@ -35,7 +35,7 @@ internal extension RFCOMMDevice {
         self.flags = .init(rawValue: cValue.flags)
         self.state = .init(rawValue: cValue.state) ?? .unknown
         self.source = cValue.source
-        self.destination = cValue.source
+        self.destination = cValue.destination
         self.channel = cValue.channel
     }
 }
