782e3aec46ceca07bbec81eb338302eea535164e
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-17 22:23:08 -0400

    Convert RFCOMMFlag to native OptionSet

--- Sources/BluetoothLinux/RFCOMM/RFCOMMFlag.swift
+++ Sources/BluetoothLinux/RFCOMM/RFCOMMFlag.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Alsey Coleman Miller on 27/10/21.
 //
@@ -9,10 +9,16 @@ import Bluetooth
 
 /// RFCOMM Flags
 @frozen
-public enum RFCOMMFlag: UInt32, CaseIterable, BitMaskOption {
-    
-    case reuseDLC           = 0x01 // RFCOMM_REUSE_DLC
-    case releaseOnHangup    = 0x02 // RFCOMM_RELEASE_ONHUP
-    case hangupNow          = 0x04 // RFCOMM_HANGUP_NOW
-    case serialAttached     = 0x08 // RFCOMM_TTY_ATTACHED
+public struct RFCOMMFlag: OptionSet, Equatable, Hashable, Sendable {
+
+    public let rawValue: UInt32
+
+    public init(rawValue: UInt32) {
+        self.rawValue = rawValue
+    }
+
+    public static let reuseDLC          = RFCOMMFlag(rawValue: 0x01) // RFCOMM_REUSE_DLC
+    public static let releaseOnHangup   = RFCOMMFlag(rawValue: 0x02) // RFCOMM_RELEASE_ONHUP
+    public static let hangupNow         = RFCOMMFlag(rawValue: 0x04) // RFCOMM_HANGUP_NOW
+    public static let serialAttached    = RFCOMMFlag(rawValue: 0x08) // RFCOMM_TTY_ATTACHED
 }
